71c4f9867a6626a9e16ba5ffe866a31bcb39e5cf
ahrke <david.duy.minh@gmail.com>
2019-07-30 17:25:50 +0000

    added timer1 with functionality

---
+++ timer1.js
@@ -0,0 +1,14 @@
+const args = process.argv.slice(2);
+
+const timer = (delay) => {
+  
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, delay * 1000)
+}
+
+args.forEach(e => {
+  if (!isNaN(e) && (e > 0)) {
+    timer(e)
+  } 
+}) 
